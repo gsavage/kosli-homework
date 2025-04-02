d9cd184230fcbd7493756dd0d7b390b04dc4e8ff
Graham Savage <grahamsavage@fastmail.com>
2025-04-02 22:58:09 +0100

    Add bootstrap steps for IAM
    Updated the iam policy so that the bucket name can be provided to it
    and updated the bootstrap script to fill in the details.  The script
    now also creates the user and an access key.
    
    At this point, once the file has been run, I have enough pieces to
    have a backend setup.

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -2,3 +2,12 @@
 
 aws s3api create-bucket --acl private --bucket ${BUCKET_NAME} --create-bucket-configuration LocationConstraint=eu-west-2 --region eu-west-2
 
+aws iam create-user --user-name kosli
+
+sed -e "s/BUCKET_NAME/${BUCKET_NAME}/" tf-iam.policy.json.template > tf-iam.policy.json
+
+aws iam create-policy --policy-name kosli-tf-policy --policy-document file://./tf-iam.policy.jso
+
+aws iam create-access-key --user-name kosli
+
+echo "Now, take these credentials and put into your aws credentials file"
