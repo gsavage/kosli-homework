bbb0f3d1c629dbbb2d1f5be6ca2dd4437073db07
Graham Savage <grahamsavage@fastmail.com>
2025-04-03 23:06:48 +0100

    Upload files to the bucket
    Using the fileset is a mechanism for Terraform itself to upload content
    to the bucket.  If an HTML file were to be changed, running terraform apply
    again would upload the new file.

--- website-bucket.tf
+++ website-bucket.tf
@@ -1,3 +1,13 @@
 resource "aws_s3_bucket" "static_site" {
   bucket = "gsavage-kosli-static-site-${var.env}"
 } 
+
+resource "aws_s3_object" "upload_content" {
+  bucket = "gsavage-kosli-static-site-${var.env}"
+
+  for_each = fileset("website/", "**/*")
+
+  key = each.value
+  source = "website/${each.value}"
+  etag = filemd5("website/${each.value}")
+}
