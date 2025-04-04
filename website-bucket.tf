c697bad3fe2199de27bc56b5b3ea0ec85d3cd288
Graham Savage <grahamsavage@fastmail.com>
2025-04-04 22:34:35 +0100

    Improve the s3 object upload
    Set the content type, badly, so that when looking at the pages in a browser
    it works as expected.
    
    Additionally, remove the duplication of the bucket name, and instead use the
    one from the bucket itself.

--- website-bucket.tf
+++ website-bucket.tf
@@ -7,11 +7,13 @@ resource "aws_s3_bucket" "static_site" {
 # Should Terraform upload the website files?
 #
 resource "aws_s3_object" "upload_content" {
-  bucket = "gsavage-kosli-static-site-${var.env}"
+  bucket = aws_s3_bucket.static_site.id
+  depends_on = [aws_s3_bucket.static_site]
 
   for_each = var.upload_files_with_terraform ? fileset("website/", "**/*") : []
 
   key    = each.value
   source = "website/${each.value}"
+  content_type = each.value
   etag   = filemd5("website/${each.value}")
 }
