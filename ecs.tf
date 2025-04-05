778c00a8d8443f033afd7a547517eb3c86ea9dc2
Graham Savage <grahamsavage@fastmail.com>
2025-04-05 23:15:02 +0100

    Create the ECS service
    The service, task definition, cluster are all created.

---
+++ ecs.tf
@@ -0,0 +1,58 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = "kosli-${var.env}-cluster"
+}
+
+resource "aws_iam_role" "allow_ecs_access" {
+  name               = "ecs-access-${var.env}"
+  assume_role_policy = data.aws_iam_policy_document.allow_ecs_access.json
+}
+
+data "aws_iam_policy_document" "allow_ecs_access" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "kosli-app-${var.env}"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.allow_ecs_access.arn
+  container_definitions = jsonencode([
+    {
+      name      = "kosli_app"
+      image     = "359024362939.dkr.ecr.eu-west-2.amazonaws.com/kosli-site:app"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+      }]
+    }
+  ])
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+  }
+
+}
+
+resource "aws_ecs_service" "nginx_service" {
+  name            = "kosli-${var.env}-nginx-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  desired_count   = 0
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets = [aws_subnet.a.id, aws_subnet.b.id]
+  }
+
+}
